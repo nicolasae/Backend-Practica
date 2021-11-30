89aa3414bad6413add7e7f034305716afc10829c
nicolas aguirre <nicolas.aguirre@utp.edu.co>
2021-11-29 21:33:15 -0500

    allowed all host

--- djangoreactproject/settings.py
+++ djangoreactproject/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = (
        'https://localhost:3000',
